50ec8a9c8ebc1394c3a15baaa73b5cd977dcd662
Joe <josephc.lambert@gmail.com>
2019-10-11 09:13:48 -0700

    wraps app in provider and injects store.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from './reducers';
+
+import './index.css';
+import App from './components/App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
+);
