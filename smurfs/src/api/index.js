126fdeba506fafeda57b1e296d029397cf0c6228
Joe <josephc.lambert@gmail.com>
2019-10-11 08:55:23 -0700

    adds index to api

--- smurfs/src/api/index.js
+++ smurfs/src/api/index.js
@@ -1,15 +1,1 @@
-export const baseURL = `localhost:3333`;
-export const SERVICES = {
-  GET: {
-    smurfs: `smurfs`,
-  },
-  POST: {
-    smurfs: `smurfs`,
-  },
-  PUT: {
-    smurfs: `smurfs`,
-  },
-  DELETE: {
-    smurfs: `smurfs`,
-  },
-};
+export { baseURL, SERVICES } from './urls';
---
+++ smurfs/src/api/urls.js
@@ -0,0 +1,15 @@
+export const baseURL = `localhost:3333`;
+export const SERVICES = {
+  GET: {
+    smurfs: `smurfs`,
+  },
+  POST: {
+    smurfs: `smurfs`,
+  },
+  PUT: {
+    smurfs: `smurfs`,
+  },
+  DELETE: {
+    smurfs: `smurfs`,
+  },
+};
