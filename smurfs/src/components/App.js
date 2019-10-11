ff84f8ee2cbe3455b14dd89c6cf38548312a94a9
Joe <josephc.lambert@gmail.com>
2019-10-11 09:25:50 -0700

    scaffolds smurf village dashboard.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-
+import SmurfsDashboard from './SmurfsDashboard';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
-    </div>
+    <>
+      <SmurfsDashboard />
+    </>
   );
 };
 
---
+++ smurfs/src/components/SmurfsDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SmurfsDashboard = () => {
+  return (
+    <section>
+      <h1>Redux Smurf Village</h1>
+      <div>create smurf form goes here</div>
+      <div>smurf list goes here</div>
+    </section>
+  );
+};
+
+export default SmurfsDashboard;
