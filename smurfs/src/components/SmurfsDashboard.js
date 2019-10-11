222687a476f587633139feafb79f77c4541cd882
Joe <josephc.lambert@gmail.com>
2019-10-11 09:53:22 -0700

    adds some layout structure.

--- smurfs/src/components/SmurfsDashboard.js
+++ smurfs/src/components/SmurfsDashboard.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import SmurfsContainer from './SmurfsContainer';
+
+const StyledSmurfWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 const SmurfsDashboard = () => {
   return (
     <section>
       <h1>Redux Smurf Village</h1>
-      <div>create smurf form goes here</div>
-      <div>
+      <StyledSmurfWrapper>
+        <div>create smurf form goes here</div>
         <SmurfsContainer />
-      </div>
+      </StyledSmurfWrapper>
     </section>
   );
 };
