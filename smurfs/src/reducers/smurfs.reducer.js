f53165d3f4249047447245d7609e7bdb51be6e38
Joe <josephc.lambert@gmail.com>
2019-10-11 09:05:02 -0700

    creates initial state and smurfs reducer for handling get request.

---
+++ smurfs/src/reducers/smurfs.reducer.js
@@ -0,0 +1,26 @@
+export { START_FETCHING_SMURFS, SMURFS_FETCH_SUCCESS, SMURFS_FETCH_FAILURE } from '../actions';
+
+import { initialState } from './initialState';
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case START_FETCHING_SMURFS:
+      console.log(START_FETCHING_SMURFS);
+      return {
+        ...state,
+      };
+    case SMURFS_FETCH_SUCCESS:
+      console.log(SMURFS_FETCH_SUCCESS);
+      return {
+        ...state,
+      };
+    case SMURFS_FETCH_FAILURE:
+      console.log(SMURFS_FETCH_FAILURE);
+      return {
+        ...state,
+      };
+    default:
+      console.log('SMURFS reducer default', state);
+      return state;
+  }
+};
